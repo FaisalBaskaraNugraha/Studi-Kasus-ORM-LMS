79d995c6c3fc626c52f8878d8d6e10e9f7d4941f
FaisalBaskaraNugraha <111202214468@mhs.dinus.ac.id>
2025-04-30 22:04:03 +0700

    Perbaikan settings dan Awal Views

--- code/core/views.py
+++ code/core/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Selamat datang di LMS KITA</h1>")
